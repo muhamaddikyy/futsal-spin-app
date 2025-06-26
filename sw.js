e90e1dab70660d85948098e56b1b00e1a52cf58e
mhmddikyy13 <muhamad.diky@students.utdi.ac.id>
2025-06-26 08:15:41 +0700

    🚀 Final versi lengkap semua fitur

--- sw.js
+++ sw.js
@@ -1,21 +1,39 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("futsal-spin-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
-      ]);
+const cacheName = 'futsal-spin-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/ig.svg',
+  '/icons/tiktok.svg',
+  '/icons/wa.svg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+      );
+    })
   );
 });
